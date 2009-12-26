5c031b19febf6e25ef3be089a3bc08f6d5dfe72f
DDLshack <ddlshack@gmail.com>
2009-12-26 03:32:09 +0000

    made modules work

--- includes/gcddl_modules.php
+++ includes/gcddl_modules.php
@@ -1,13 +1,13 @@
 <?php
 
 class Modules {
-	#private var $modules = array();
+	var $modules = array();
 	
 	function register_module($name) {
 		if(!array_key_exists($name, $this->modules)) {
-			$realname='gcddl_'.$name;
+			eval("\$handle = new gcddl_$name();");
 			$this->modules[$name]=array(
-	#			'handle' => new $realname();
+				'handle' => $handle;
 			);
 		}
 	}
